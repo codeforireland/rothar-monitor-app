ec46d0c2f2d808092b8dc4ea99a4d701953694a9
Adam Bednarski <adam.bednarski@globoforce.com>
2014-11-24 11:15:07 +0000

    ufff ...

--- src/eu/appbucket/monitor/update/StolenBikeDao.java
+++ src/eu/appbucket/monitor/update/StolenBikeDao.java
@@ -16,16 +16,26 @@ public class StolenBikeDao {
 		values.put(StolenBikeDbHelper.COLUMN_NAME_UUID, record.getUudi());
 		values.put(StolenBikeDbHelper.COLUMN_NAME_MAJOR, record.getMajor());
 		values.put(StolenBikeDbHelper.COLUMN_NAME_MINOR, record.getMinor());
-		db.insert(
-				StolenBikeDbHelper.TABLE_NAME,
-				null,
-		        values);
+		try {
+			db.insert(
+					StolenBikeDbHelper.TABLE_NAME,
+					null,
+			        values);	
+		} finally {
+			db.close();
+		}
+		
 	}
 	
 	public void resetStolenBikes(Context context) {
 		StolenBikeDbHelper dbHelper = new StolenBikeDbHelper(context);
 		SQLiteDatabase db = dbHelper.getWritableDatabase();
-		db.delete(StolenBikeDbHelper.TABLE_NAME, null, null);
+		try {
+			db.delete(StolenBikeDbHelper.TABLE_NAME, null, null);	
+		} finally {
+			db.close();
+		}
+		
 	}
 	
 	public BikeBeacon findBikeRecordByIdentity(Context context, String uuid, int major, int minor) {
@@ -46,26 +56,34 @@ public class StolenBikeDao {
 				uuid,
 				Integer.toString(major),
 				Integer.toString(minor)};
-		Cursor cursor = db.query(
-				StolenBikeDbHelper.TABLE_NAME,	// The table to query
-			    projection, 					// The columns to return
-			    selection, 						// A filter declaring which rows to return, formatted as an SQL WHERE clause (excluding the WHERE itself). 
-			    								// Passing null will return all rows for the given table.
-			    selectionArgs, 					// The values for the WHERE clause
-			    "", 							// don't group the rows
-			    "", 							// don't filter by row groups
-			    "" 								// The sort order
-		);
-		cursor.moveToFirst();
-		if(cursor.getCount() > 0) {
-			record.setAssetId(cursor.getInt(
-					cursor.getColumnIndexOrThrow(StolenBikeDbHelper.COLUMN_NAME_ASSET_ID)));
-			record.setUudi(cursor.getString(
-					cursor.getColumnIndexOrThrow(StolenBikeDbHelper.COLUMN_NAME_UUID)));
-			record.setMajor(cursor.getInt(
-					cursor.getColumnIndexOrThrow(StolenBikeDbHelper.COLUMN_NAME_MAJOR)));
-			record.setMinor(cursor.getInt(
-					cursor.getColumnIndexOrThrow(StolenBikeDbHelper.COLUMN_NAME_MINOR)));
+		Cursor cursor = null;
+		try {
+			cursor = db.query(
+					StolenBikeDbHelper.TABLE_NAME,	// The table to query
+				    projection, 					// The columns to return
+				    selection, 						// A filter declaring which rows to return, formatted as an SQL WHERE clause (excluding the WHERE itself). 
+				    								// Passing null will return all rows for the given table.
+				    selectionArgs, 					// The values for the WHERE clause
+				    "", 							// don't group the rows
+				    "", 							// don't filter by row groups
+				    "" 								// The sort order
+			);
+			cursor.moveToFirst();
+			if(cursor.getCount() > 0) {
+				record.setAssetId(cursor.getInt(
+						cursor.getColumnIndexOrThrow(StolenBikeDbHelper.COLUMN_NAME_ASSET_ID)));
+				record.setUudi(cursor.getString(
+						cursor.getColumnIndexOrThrow(StolenBikeDbHelper.COLUMN_NAME_UUID)));
+				record.setMajor(cursor.getInt(
+						cursor.getColumnIndexOrThrow(StolenBikeDbHelper.COLUMN_NAME_MAJOR)));
+				record.setMinor(cursor.getInt(
+						cursor.getColumnIndexOrThrow(StolenBikeDbHelper.COLUMN_NAME_MINOR)));
+			}	
+		} finally {
+			if(cursor != null && !cursor.isClosed()) {
+				cursor.close();
+			}
+			db.close();
 		}
 		return record;
 	}
