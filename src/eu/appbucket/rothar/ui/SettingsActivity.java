a1c837be11e9b1d760c02136103988da09437243
Adam Bednarski <abednarski79@gmail.com>
2015-02-15 22:26:58 +0000

    proper settings early steps

--- src/eu/appbucket/rothar/ui/SettingsActivity.java
+++ src/eu/appbucket/rothar/ui/SettingsActivity.java
@@ -2,23 +2,22 @@ package eu.appbucket.rothar.ui;
 
 
 import android.app.Activity;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
-import eu.appbucket.rothar.R;
+import android.preference.EditTextPreference;
+import android.preference.Preference;
+import android.preference.PreferenceManager;
 
-/*public class SettingsActivity extends PreferenceActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.about);
-    }
-}*/
-
-public class SettingsActivity extends Activity {
+public class SettingsActivity extends Activity  {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        Editor editor = sharedPrefs.edit();
+		editor.putString("pref_syncConnectionType", "15");
+		editor.commit();
         // Display the fragment as the main content.
         getFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new SettingsFragment())
--- src/eu/appbucket/rothar/ui/SettingsFragment.java
+++ src/eu/appbucket/rothar/ui/SettingsFragment.java
@@ -1,16 +1,30 @@
 package eu.appbucket.rothar.ui;
 
-import eu.appbucket.rothar.R;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import eu.appbucket.rothar.R;
 
-public class SettingsFragment extends PreferenceFragment {
+public class SettingsFragment extends PreferenceFragment implements OnSharedPreferenceChangeListener {
 	
 	@Override
     public void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
-
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.about);
     }
+	
+	@Override
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+			String key) {
+		Preference pref = findPreference(key);
+        if (pref instanceof EditTextPreference) {
+            EditTextPreference etp = (EditTextPreference) pref;
+            pref.setSummary(etp.getText());
+        }
+		
+	}
 }
